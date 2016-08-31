b38e2812e36108177f5556c2e17b073d8885b9b6
westhues <matthias.westhues@uni-hohenheim.de>
2016-08-31 08:38:16 +0200

    Save imputed and SNP-ETA objects

--- analysis/create_ETA.R
+++ analysis/create_ETA.R
@@ -17,36 +17,47 @@ mrna <- mrna[grep("Exp|Sigma", x = rownames(mrna), invert = TRUE), ]
 mrna <- mrna[rownames(mrna) %in% rownames(snp), ]
 
 
-imputation <- TRUE
 
 # Build a kernel from the genomic data
 hetgrps <- c("Dent", "Flint")
-if (isTRUE(imputation)) {
-  ETA <- lapply(hetgrps, FUN = function(hetgrp) {
-    grp_geno <- genos[[hetgrp]][["snp"]]
-    x <- snp[rownames(snp) %in% grp_geno, ]
-    y <- mrna[rownames(mrna) %in% grp_geno, ]
-    hyb_id <- ifelse(hetgrp == "Dent", yes = 2, no = 3)
-    geno <- vapply(strsplit(genos$Hybrid, split = "_"), FUN = "[[", hyb_id, 
-                   FUN.VALUE = character(1))
-    impute_eta(x = x,
-               y = y,
-               geno = geno,
-               bglr_model = "BRR")
+imp_eta <- lapply(hetgrps, FUN = function(hetgrp) {
+  grp_geno <- genos[[hetgrp]][["snp"]]
+  x <- snp[rownames(snp) %in% grp_geno, ]
+  y <- mrna[rownames(mrna) %in% grp_geno, ]
+  hyb_id <- ifelse(hetgrp == "Dent", yes = 2, no = 3)
+  geno <- vapply(strsplit(genos$Hybrid, split = "_"), FUN = "[[", hyb_id, 
+                 FUN.VALUE = character(1))
+  eta <- impute_eta(x = x,
+                    y = y,
+                    geno = geno,
+                    bglr_model = "BRR")
+  eta[] <- lapply(seq_along(eta), FUN = function(i) {
+    x <- eta[[i]][["X"]]
+    rownames(x) <- genos$Hybrid
+    x
   })
-} else {
-  ETA <- lapply(hetgrps, FUN = function(hetgrp) {
-    grp_geno <- genos[[hetgrp]][["snp"]]
-    x <- snp[rownames(snp) %in% grp_geno, ]
-    hyb_id <- ifelse(hetgrp == "Dent", yes = 2, no = 3)
-    geno <- vapply(strsplit(genos$Hybrid, split = "_"), FUN = "[[", hyb_id, 
-                   FUN.VALUE = character(1))
-    complete_eta(x = x, 
-                 geno = geno, 
-                 bglr_model = "BRR")
-  })
-}
-ETA <- unlist(ETA, recursive = FALSE)
+  eta
+})
+imp_eta <- unlist(imp_eta, recursive = FALSE)
+saveRDS(imp_eta, file = "./data/processed/imputed_ETA.RDS", compress = FALSE)
 
+snp_eta <- lapply(hetgrps, FUN = function(hetgrp) {
+  grp_geno <- genos[[hetgrp]][["snp"]]
+  x <- snp[rownames(snp) %in% grp_geno, ]
+  hyb_id <- ifelse(hetgrp == "Dent", yes = 2, no = 3)
+  geno <- vapply(strsplit(genos$Hybrid, split = "_"), FUN = "[[", hyb_id, 
+                 FUN.VALUE = character(1))
+  eta <- complete_eta(x = x, 
+                      geno = geno, 
+                      bglr_model = "BRR")
+  eta[] <- lapply(seq_along(eta), FUN = function(i) {
+    x <- eta[[i]][["X"]]
+    rownames(x) <- genos$Hybrid
+    x
+  })
+  eta
+})
+snp_eta <- unlist(snp_eta, recursive = FALSE)
+saveRDS(snp_eta, file = "./data/processed/snp_ETA.RDS", compress = FALSE)
 
 
