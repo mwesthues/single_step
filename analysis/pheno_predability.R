0f624a05c611d745e22bb98b43411a87aa60b845
westhues <matthias.westhues@uni-hohenheim.de>
2016-09-05 14:55:30 +0200

    return pred_ability for loocv and cv1000

--- analysis/pheno_predability.R
+++ analysis/pheno_predability.R
@@ -1,56 +1,22 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load("data.table", "ggplot2", "ggthemes")
+run_log <- fread("./data/processed/prediction_log/log_list.txt")
 
-fl_nms <- list.files("./data/derived/", pattern = "pheno_prediction")
-fl_lst <- lapply(seq_along(fl_nms), FUN = function(i) {
-  fl_nm <- fl_nms[i]
-  mod_pos <- regexpr("(?<=Model=).*(?=_VCOV)", text = fl_nm, perl = TRUE)
-  mod_nm <- substring(text = fl_nm, first = mod_pos,
-                      last = mod_pos + attr(mod_pos, "match.length") - 1)
-  vcov_pos <- regexpr("(?<=VCOV=).*(?=_Iter)", text = fl_nm, perl = TRUE)
-  vcov_nm <- substring(text = fl_nm, first = vcov_pos,
-                       last = vcov_pos + attr(vcov_pos, "match.length") - 1)
-  iter_pos <- regexpr("(?<=Iter=).*(?=_SnpFilter)", text = fl_nm, perl = TRUE)
-  iter_nm <- substring(text = fl_nm, first = iter_pos,
-                       last = iter_pos + attr(iter_pos, "match.length") - 1)
-  fltr_pos <- regexpr("(?<=SnpFilter=).*(?=_Imputed)", text = fl_nm, 
-                      perl = TRUE)
-  fltr_nm <- substring(text = fl_nm, first = fltr_pos,
-                       last = fltr_pos + attr(fltr_pos, "match.length") - 1)
-  imp_pos <- regexpr("(?<=Imputed=).*(?=_Comparison)", text = fl_nm, 
-                      perl = TRUE)
-  imp_nm <- substring(text = fl_nm, first = imp_pos,
-                      last = imp_pos + attr(imp_pos, "match.length") - 1)
-  cmp_pos <- regexpr("(?<=Comparison=).*(?=.RDS)", text = fl_nm, 
-                      perl = TRUE)
-  cmp_nm <- substring(text = fl_nm, first = cmp_pos,
-                      last = cmp_pos + attr(cmp_pos, "match.length") - 1)
-  dat <- readRDS(paste0("./data/derived/", fl_nm))
-  dat[, `:=`(Model = mod_nm,
-             VCOV = vcov_nm,
-             Iter = iter_nm,
-             SNP_Filter = fltr_nm,
-             Imputed = imp_nm,
-             Comparison = cmp_nm), ]
 
-  dat
+# CV1000
+cv1000_nms <- run_log[CV != "LOOCV", Job_ID, ]
+cv1000 <- lapply(seq_along(cv1000_nms), FUN = function(i) {
+  readRDS(paste0("./data/processed/predictions/", cv1000_nms[i], ".RDS"))
 })
-DT <- rbindlist(fl_lst)
-pred_ability <- DT[, .(PredAbility = cor(y, yhat)), 
-                   by = .(Phenotype, Predictor, Model, VCOV, Iter, SNP_Filter,
-                          Imputed, Comparison)]
-pred_ability <- pred_ability[!(Predictor == "mrna" & Comparison == "mrna"), , ]
+cv1000 <- rbindlist(cv1000)
+cv1000[, mean(Pred_Ability), by = .(Imputation)]
 
 
-p1 <- ggplot(pred_ability, aes(x = Phenotype, y = PredAbility, 
-                               fill = Predictor)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  theme_pander(base_size = 8) +
-  facet_grid(. ~ Imputed) +
-  theme(legend.position = "top",
-        axis.text.x = element_text(angle = 60, hjust = 1, vjust = 1)) +
-  xlab("Trait") +
-  ylab(bquote(r))
-ggsave("./tabs_figs/pheno_predability-barplot.pdf", width = 4, height = 4, 
-       plot = p1)
+# LOOCV 
+loocv_nms <- run_log[CV == "LOOCV", Job_ID, ]
+loocv <- lapply(seq_along(loocv_nms), FUN = function(i) {
+  readRDS(paste0("./data/processed/predictions/", loocv_nms[i], ".RDS"))
+})
+loocv <- rbindlist(loocv)
+loocv[, mean(cor(y, yhat)), by = .(Imputation)]
 
