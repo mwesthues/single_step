7376d10526b62b1cf6681b563ff8beb408f891ca
westhues <matthias.westhues@uni-hohenheim.de>
2017-03-15 15:55:54 +0100

    UHOH PCA with hybrid, no plotting

---
+++ analysis/uhoh_pca.R
@@ -0,0 +1,160 @@
+# Goal: Run a pricipal component analysis on the SNP marker data of the 
+# parental inbred lines of the UHOH maize hybrids.
+# Use the plot of the first two principal components to assess how well the 
+# total genetic space of the material is covered by genotypes that have SNP and
+# mRNA data.
+
+# Data and packages -------------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load("tidyverse", "LEA", "stringr", "viridis", "devtools")
+devtools::install_github("mwesthues/sspredr")
+pacman::p_load_gh("mwesthues/sspredr")
+
+
+# ************************ PARENTAL INBRED LINES ***************************
+# Group-wise SNP quality checks.
+# Load SNP marker data, which were imputed using Beagle.
+snp <- "./data/processed/uhoh/imputed_snp_mat.RDS" %>%
+  readRDS()
+rownames(snp) <- gsub("X", replacement = "", x = rownames(snp))
+
+# Data frame with information group membership (Dent or Flint) of genotypes and
+# with information on predictor coverage of genotypes.
+common_genotypes <- "./data/processed/common_genotypes.RDS" %>%
+  readRDS()
+  
+# For each heterotic group, apply quality checks to the SNP data and keep only
+# markers that pass those criteria.
+quality_snps <- common_genotypes %>%
+  filter(Data_Type == "snp") %>%
+  split(.$Pool) %>%
+  map("G") %>%
+  map(function(x) {
+    snp[rownames(snp) %in% x, ]
+  }) %>%
+  map(., ~ensure_snp_quality(
+    ., callfreq_check = FALSE, maf_check = TRUE, maf_threshold = 0.05,
+    any_missing = FALSE, remove_duplicated = TRUE
+  ))
+
+
+# Save the genotypic data to files so that the PCA can be run with the pca 
+# function from the LEA package.
+lfmm_path <- "./data/derived/uhoh/"
+quality_snps %>%
+  seq_along() %>%
+  map(function(i) {
+    x <- quality_snps[[i]]
+    x_nm <- names(quality_snps)[i]
+    write.lfmm(x, paste0(lfmm_path, "snp_", x_nm, ".lfmm"))
+  })
+
+# Determine the structure of the data using genotypic data.
+pca_lst <- lfmm_path %>%
+  list.files(pattern = "lfmm") %>%
+  map(., ~paste0(lfmm_path, .)) %>%
+  map(., ~LEA::pca(input.file = ., scale = TRUE)) %>%
+  map(function(x) x$projections) %>%
+  map2(.y = quality_snps, .f = function(x, y) {
+    rownames(x) <- rownames(y)
+    x
+  })
+
+# Get the names of genotypes that are covered only by mRNA data and the ones
+# that are covered by both, SNP and mRNA data.
+# These information will be required for assigning genotypes to the complete 
+# (i.e., SNP) and the incomplete (i.e., mRNA) predictor in the PCA.
+predictor_coverage <- common_genotypes %>%
+  filter(Data_Type %in% c("snp", "mrna")) %>%
+  split(.$Data_Type) %>%
+  map("G")
+
+# Convert the PCA list for Dent and Flint material to a data frame and add 
+# information on predictor coverage of the genotypes.
+pca_df <- pca_lst %>%
+  map(as.data.frame) %>%
+  map(., ~rownames_to_column(., var = "G")) %>%
+  map(., ~gather(., key = PC, value = Score, -G)) %>%
+  set_names(names(quality_snps)) %>%
+  bind_rows(.id = "Group") %>%
+  as_data_frame() %>%
+  mutate(PC = str_replace_all(PC, pattern = "V", replacement = "PC")) %>%
+  filter(PC %in% paste0("PC", seq_len(2))) %>%
+  mutate(
+    Reduced = if_else(
+      G %in% predictor_coverage$mrna,
+      true = TRUE,
+      false = FALSE
+    )
+  )
+
+
+
+# *******************************  HYBRIDS ********************************
+hybrid_parents <- common_genotypes %>%
+  filter(Data_Type == "snp") %>%
+  split(.$Pool) %>%
+  map("G") %>%
+  reduce(union)
+
+hyb_lfmm_path <- "./data/derived/uhoh/snp_Hybrid.lfmm"
+hybrid_snp <- snp %>%
+  .[rownames(.) %in% hybrid_parents, ] %>%
+  ensure_snp_quality(
+    ., callfreq_check = FALSE, maf_check = TRUE, maf_threshold = 0.05,
+    any_missing = FALSE, remove_duplicated = TRUE
+  )
+write.lfmm(hybrid_snp, hyb_lfmm_path)
+
+
+# Determine the structure of the data using genotypic data.
+hyb_pca_mat <- hyb_lfmm_path %>%
+  LEA::pca(input.file = ., scale = TRUE) %>%
+  .$projections 
+rownames(hyb_pca_mat) <- rownames(hybrid_snp)
+
+
+# Convert the PCA list for Dent and Flint material to a data frame and add 
+# information on predictor coverage of the genotypes.
+parental_dent_lines <- common_genotypes %>%
+  filter(Data_Type == "snp",
+         Pool == "Dent") %>%
+  select(G) %>%
+  flatten_chr()
+
+hybrid_pca_df <- hyb_pca_mat %>%
+  as.data.frame() %>%
+  rownames_to_column(., var = "G") %>%
+  gather(., key = PC, value = Score, -G) %>%
+  as_data_frame() %>%
+  mutate(PC = str_replace_all(PC, pattern = "V", replacement = "PC")) %>%
+  filter(PC %in% paste0("PC", seq_len(2))) %>%
+  mutate(
+    Group = if_else(
+      G %in% parental_dent_lines,
+      true = "Dent",
+      false = "Flint"
+    ),
+    Type = "Hybrid",
+    Reduced = FALSE
+  ) 
+pca_df$Type <- "Inbred"
+
+
+
+
+
+# PCA plot
+g1 <- pca_df %>%
+  spread(key = PC, value = Score) %>%
+  ggplot(aes(x = PC1, y = PC2, color = Reduced, shape = Reduced)) +
+  geom_point() +
+  scale_color_tableau() +
+  facet_grid(. ~ Group) +
+  theme_bw(base_size = 10) +
+  theme(legend.position = "top")
+ggsave(plot = g1, 
+       filename = "./paper/tables_figures/uhoh_dent_flint_pca.pdf",
+       height = 4, 
+       width = 6,
+       units = "in")
