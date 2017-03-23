2ca050e8f5fd4fd56c1a20586d34aed3ef280822
westhues <matthias.westhues@uni-hohenheim.de>
2017-03-23 11:18:11 +0100

    Restrict admixture to 149 core genotypes

--- analysis/maizego_snp_analyses.R
+++ analysis/maizego_snp_analyses.R
@@ -3,12 +3,21 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load("tidyverse", "LEA", "stringr", "viridis", "cowplot", 
                "ggthemes")
 
+# For the second scenario, keep only names of genotypes, which are covered by 
+# all data types (i.e. phenotypic, genotypic and transcriptomic).
+common_genotypes <- readRDS(
+  "./data/derived/maizego/unique_snp-mrna-pheno_genotypes.RDS"
+)
+common_genotypes <- common_genotypes %>%
+  reduce(intersect)
 
-snp <- readRDS("./data/processed/maizego/imputed_snp_mat.RDS")
-unique_genotypes <- "./data/derived/maizego/unique_snp-mrna-pheno_genotypes.RDS" %>%
+snp <- "./data/processed/maizego/imputed_snp_mat.RDS" %>%
   readRDS() %>%
-  keep(names(.) == "snp") %>%
-  flatten_chr()
+  .[rownames(.) %in% common_genotypes, ] %>%
+  sspredr::ensure_snp_quality(
+    ., callfreq_check = FALSE, maf_check = TRUE, maf_threshold = 0.05, 
+    any_missing = FALSE, remove_duplicated = TRUE
+  )
 
 
 # PCA ---------------------------------------------------------------------
@@ -36,12 +45,14 @@ snp_tw %>%
 # subpopulations.
 lfmm2geno("./data/derived/maizego/snp.lfmm", 
           output.file = "./data/derived/maizego/snp.geno")
-project <- snmf(input.file = "./data/derived/maizego/snp.geno",
-                K = seq_len(tw_K),
-                project = "new",
-                repetitions = 25,
-                CPU = 3,
-                entropy = TRUE)
+# This code snippet is commented out because it takes a long time to run and 
+# can easily and inadvertantly be overridden.
+#project <- snmf(input.file = "./data/derived/maizego/snp.geno",
+#                K = seq_len(tw_K),
+#                project = "new",
+#                repetitions = 25,
+#                CPU = 3,
+#                entropy = TRUE)
 
 # Based on the results of a PCA, decide whether it is even necessary to treat
 # subpopulations as such or whether all inbred lines can be treated as a single
@@ -50,13 +61,6 @@ project <- load.snmfProject("./data/derived/maizego/snp.snmfProject")
 
 
 # SNP VS MRNA PCA ---------------------------------------------------------
-# For the second scenario, keep only names of genotypes, which are covered by 
-# all data types (i.e. phenotypic, genotypic and transcriptomic).
-common_genotypes <- readRDS(
-  "./data/derived/maizego/unique_snp-mrna-pheno_genotypes.RDS"
-)
-common_genotypes <- common_genotypes %>%
-  reduce(intersect)
 
 # Explore the kinship matrix.
 all_geno_snp <- "./data/processed/maizego/imputed_snp_mat.RDS" %>%
@@ -123,23 +127,24 @@ structure_df <- K_lst %>%
     x
   }) %>%
   map(., ~gather(., key = Component, value = Value, -G)) %>%
-  bind_rows(.id = "K")
-
-# For each genotype and for each run with K ancestral populations, plot the 
-# admixture coefficients by genotype.
-g2 <- structure_df %>%
+  bind_rows(.id = "K") %>%
   mutate(
     K = as.numeric(K),
     K = K + 1,
     K = paste0("K=", as.character(K))
   ) %>%
-  mutate_at(vars(K, G, Component), as.factor) %>%
+  mutate_at(vars(K, G, Component), as.factor)
+
+# For each genotype and for each run with K ancestral populations, plot the 
+# admixture coefficients by genotype.
+g2 <- structure_df %>%
   ggplot(aes(x = G, y = Value, fill = Component)) +
   geom_bar(stat = "identity", width = 1) +
   facet_wrap(~K, ncol = 1, strip.position = "right") +
   scale_fill_viridis(discrete = TRUE) +
   ggthemes::theme_pander(base_size = 10) +
   theme(axis.text.x = element_blank(),
+        #legend.position = "top") +
         legend.position = "none") +
   xlab("Genotype") +
   ylab("Ancestry Coefficient")
@@ -152,12 +157,23 @@ g3 <- pc_df %>%
   filter(Fraction != "1.0") %>%
   rename(`Core Group` = Core_Group) %>%
   ggplot(aes(x = PC1, y = PC2, color = `Core Group`, shape = `Core Group`)) +
-  geom_point(size = 0.5) +
+  geom_point(size = 1) +
   guides(color = guide_legend(override.aes = list(size = 3))) +
   ggthemes::scale_color_tableau() +
   facet_wrap(~Fraction, nrow = 3, ncol = 3) +
   theme_bw(base_size = 10) +
-  theme(legend.position = "top")
+  theme(
+    legend.position = "top",
+    strip.background = element_blank(),
+    strip.placement = "outside"
+  )
+
+ggsave(plot = g3,
+       filename = "./paper/tables_figures/maizego_core_sampling_pca.pdf",
+       width = 7,
+       height = 7,
+       units = "in"
+       )
 
 
 g23 <- plot_grid(g2, g3, labels = c("A", "B"), ncol = 2, nrow = 1)
@@ -166,3 +182,79 @@ ggsave(plot = g23,
        width = 7,
        height = 4,
        units = "in")
+
+
+
+
+# ALTERNATIVE CORE SAMPLING -----------------------------------------------
+# Select genotypes whose ancestry coefficients indicate a share of the second 
+# ancestral population is equal to or greater than 0.5.
+# greater than 
+ancestry_df <- structure_df %>%
+  filter(K == "K=3") %>%
+  droplevels() %>%
+  split(.$Component) %>%
+  map(droplevels) %>%
+  map(., ~filter(., Value >= 0.5)) %>%
+  map(., ~select(., G)) %>%
+  bind_rows(.id = "Ancestor") %>%
+  mutate(
+    G = as.character(G),
+    Ancestor = gsub(pattern = "V", replacement = "A", x = Ancestor)
+  )
+ancestry_df %>%
+  saveRDS(file = "./data/derived/maizego/ancestry_k3.RDS")
+ 
+
+g4 <- structure_df %>%
+  filter(K == "K=3") %>%
+  mutate_at(vars(K, G, Component), .funs = funs(as.character)) %>%
+  mutate(Component = gsub(pattern = "V", replacement = "A", x = Component)) %>% 
+  ggplot(aes(x = G, y = Value, fill = Component)) +
+  geom_bar(stat = "identity", width = 1) +
+  facet_wrap(~K, ncol = 1) +
+  scale_fill_viridis(discrete = TRUE) +
+  ggthemes::theme_pander(base_size = 10) +
+  theme(
+    axis.text.x = element_blank(),
+    legend.position = "none",
+    strip.background = element_blank(),
+    strip.text.x = element_blank()
+  ) +
+  xlab("Genotype") +
+  ylab("Ancestry Coefficient")
+
+
+g5 <- pc_df %>% 
+  filter(Fraction == "1.0") %>% 
+  full_join(y = ancestry_df, by = "G") %>%
+  mutate(Ancestor = if_else(
+    is.na(Ancestor), true = "mixed", false = Ancestor
+  )) %>% 
+  ggplot(aes(x = PC1, y = PC2, color = Ancestor, shape = Ancestor)) +
+  geom_point(size = 2) +
+  scale_color_manual(values = c(viridis(n = 3), "red")) +
+  scale_shape_manual(values = c(15, 17, 19, 3)) +
+  guides(color = guide_legend(override.aes = list(size = 3))) +
+  ggthemes::theme_pander(base_size = 10)
+
+
+g45_wo_legend <- plot_grid(
+  g4, 
+  g5 + theme(legend.position = "none"),
+  labels = c("A", "B"),
+  ncol = 2,
+  nrow = 1
+)
+
+g45_legend <- get_legend(g5 + theme(legend.position = "top"))
+g45 <- plot_grid(g45_legend, g45_wo_legend, ncol = 1, rel_heights = c(0.15, 1))
+g45
+
+ggsave(plot = g45,
+       filename = "./paper/tables_figures/maizego_admixture_k3_pca.pdf",
+       width = 7,
+       height = 4,
+       units = "in"
+       )
+
