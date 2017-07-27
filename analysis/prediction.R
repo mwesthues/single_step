b9943e6caed281f197b3b6bac7eb8dba2a175a4e
westhues <matthias.westhues@uni-hohenheim.de>
2017-07-27 10:20:36 +0200

    Fix hybrid filtering bug in predictions

--- analysis/prediction.R
+++ analysis/prediction.R
@@ -10,7 +10,7 @@
 #
 #
 if (isTRUE(interactive())) {
-  .libPaths(c(.libPaths(), "~/R/x86_64-pc-linux-gnu-library/3.4/"))
+  .libPaths("~/R/x86_64-pc-linux-gnu-library/3.4/")
 }
 if (!require("pacman")) install.packages("pacman")
 if (!require("devtools")) install.packages("devtools")
@@ -42,7 +42,7 @@ if (isTRUE(interactive())) {
   Sys.setenv("PI" = "0.5")
   Sys.setenv("PRIOR_PI_COUNT" = "10")
   # Which interval of data shall be analyzed?
-  Sys.setenv("INTERVAL" = "2")
+  Sys.setenv("INTERVAL" = "FHN_1")
   # Number of test runs.
   Sys.setenv("RUNS" = "1-3")
   # Output directory for temporary BGLR files
@@ -77,24 +77,24 @@ original_prediction_template <- temp_loc %>%
   readRDS()
 
 prediction_template <- original_prediction_template %>%
-  filter(Interval == pred_interval) %>%
-  mutate(Rnd_Level2 = as.character(Rnd_Level2))
+  dplyr::filter(Interval == pred_interval) %>%
+  dplyr::mutate(Rnd_Level2 = as.character(Rnd_Level2))
 
 if (isTRUE(nchar(runs) != 0)) {
   run_seq <- runs %>%
     strsplit(., split = "-") %>%
-    flatten_chr() %>%
+    purrr::flatten_chr() %>%
     as.integer() %>%
     max() %>%
     seq_len()
   prediction_template <- prediction_template %>%
-    slice(run_seq)
+    dplyr::slice(run_seq)
 }
 
 # Get the individual levels of the level2 randomization procedure to load the
 # correct subset of data.
 rnd2_levels <- prediction_template %>%
-  pull(Rnd_Level2) %>% 
+  dplyr::pull(Rnd_Level2) %>% 
   unique() %>%
   as.integer()
 
@@ -112,14 +112,14 @@ if (isTRUE(all(rnd2_levels %in% seq_len(25)))) {
 # Get the log file for the ETA objects.
 eta_log <- "./data/derived/log_prepare_subsamples.txt" %>%
   readr::read_tsv() %>%
-  mutate(Pred2 = if_else(is.na(Pred2), true = "", false = Pred2))
+  dplyr::mutate(Pred2 = if_else(is.na(Pred2), true = "", false = Pred2))
 
 
 ## -- PHENOTYPIC DATA ----------------------------------------------------
 inb_pheno <- readRDS("./data/derived/maizego/tst_pheno_tibble.RDS")
 hyb_pheno <- "./data/derived/uhoh/agro_tibble.RDS" %>%
   readRDS() %>%
-  separate(
+  tidyr::separate(
     col = Genotype,
     into = c("Prefix", "Dent", "Flint"),
     sep = "_",
@@ -142,73 +142,85 @@ mclapply(pred_seq, FUN = function(i) {
 
   # Collect the current set of parameters.
   template_i <- prediction_template %>%
-    slice(i)
+    dplyr::slice(i)
 
   # Collect the current ETA object for the prediction.
   eta_i <- eta_log %>%
-    mutate(
+    dplyr::mutate(
       Rnd_Level1 = as.numeric(Rnd_Level1),
       Core_Fraction = as.character(Core_Fraction)
     ) %>%
-    semi_join(
+    dplyr::semi_join(
       y = template_i,
       by = c(
         "Extent", "Material", "Scenario", "Pred1", "Pred2", "Core_Fraction",
         "Rnd_Level1"
       )
     ) %>%
-    pull(Data_Location) %>%
+    dplyr::pull(Data_Location) %>%
     readRDS()
 
-  if (isTRUE(pull(template_i, Material) == "Hybrid")) {
+  if (isTRUE(dplyr::pull(template_i, Material) == "Hybrid")) {
+    # Remove duplicated entries. This is important for hybrids with multiple 
+    # predictors.
+    swap_logic <- purrr::compose(`!`, duplicated)
+    duplicated_filter <- eta_i %>%
+      purrr::map(.f = `[[`, "X") %>%
+      purrr::map(rownames) %>%
+      purrr::map(tibble::as_data_frame) %>%
+      dplyr::bind_cols() %>%
+      t() %>%
+      swap_logic()
+
     eta_order <- eta_i %>%
-      map(.f = `[[`, "X") %>%
-      map(rownames) %>%
-      map(as_data_frame) %>%
-      bind_cols() %>%
-      rename(Dent = "value", Flint = "value1") %>%
-      unite(col = Genotype, Dent, Flint, sep = "_") %>%
-      mutate(Genotype = paste0("DF_", Genotype))
+      .[duplicated_filter] %>%
+      purrr::map(.f = `[[`, "X") %>%
+      purrr::map(rownames) %>%
+      purrr::map(tibble::as_data_frame) %>%
+      dplyr::bind_cols() %>%
+      dplyr::rename(Dent = "value", Flint = "value1") %>%
+      tidyr::unite(col = Genotype, Dent, Flint, sep = "_") %>%
+      dplyr::mutate(Genotype = paste0("DF_", Genotype))
   } else if (isTRUE(pull(template_i, Material) == "Inbred")) {
     eta_order <- eta_i %>%
-      map(.f = `[[`, "X") %>%
-      map(rownames) %>%
-      reduce(intersect) %>%
-      as_data_frame() %>%
-      rename(Genotype = "value")
+      purrr::map(.f = `[[`, "X") %>%
+      purrr::map(rownames) %>%
+      purrr::reduce(intersect) %>%
+      tibble::as_data_frame() %>%
+      dplyr::rename(Genotype = "value")
   }
 
   # Select test and training set individuals for the current combination.
   rnd_level2_i <- rnd_level2_df %>%
-    semi_join(
+    dplyr::semi_join(
       y = template_i,
       by = c("Extent", "Material", "Scenario", "Rnd_Level2")
     ) %>%
-    select(TST_Geno, TRN_Geno)
+    dplyr::select(TST_Geno, TRN_Geno)
 
   # Vector with genotype names.
   tst_set_i <- rnd_level2_i %>%
-    pull(TST_Geno) %>%
-    unique()
+    dplyr::pull(TST_Geno) %>%
+    base::unique()
 
   # Select the correct set of phenotypes.
-  if (isTRUE(pull(template_i, Material) == "Hybrid")) {
+  if (isTRUE(dplyr::pull(template_i, Material) == "Hybrid")) {
     pheno_i <- hyb_pheno %>%
-      filter(
+      dplyr::filter(
         Trait == pull(template_i, Trait),
         Genotype %in% tst_set_i
       ) %>%
-      select(-Prefix) %>%
-      rename(Observed = "Value") %>%
-      right_join(y = eta_order, by = "Genotype")
-  } else if (isTRUE(pull(template_i, Material) == "Inbred")) {
+      dplyr::select(-Prefix) %>%
+      dplyr::rename(Observed = "Value") %>%
+      dplyr::right_join(y = eta_order, by = "Genotype")
+  } else if (isTRUE(dplyr::pull(template_i, Material) == "Inbred")) {
     pheno_i <- inb_pheno %>%
-      filter(
+      dplyr::filter(
         Trait == pull(template_i, Trait),
         Genotype %in% tst_set_i
       ) %>%
-      rename(Observed = "Value") %>%
-      right_join(y = eta_order, by = "Genotype")
+      dplyr::rename(Observed = "Value") %>%
+      dplyr::right_join(y = eta_order, by = "Genotype")
   }
 
 
@@ -217,11 +229,11 @@ mclapply(pred_seq, FUN = function(i) {
   one_param_lst <- lapply(eta_seq, FUN = function(j) {
 
     tst_geno_j <- pheno_i %>%
-      slice(j) %>%
-      pull(Genotype)
+      dplyr::slice(j) %>%
+      dplyr::pull(Genotype)
 
     pheno_j <- pheno_i %>%
-      mutate(Observed = if_else(
+      dplyr::mutate(Observed = if_else(
         Genotype == tst_geno_j,
         true = NA_real_,
         false = Observed
@@ -229,7 +241,7 @@ mclapply(pred_seq, FUN = function(i) {
     )
 
     mod_bglr <- BGLR::BGLR(
-      y = pull(pheno_j, Observed),
+      y = dplyr::pull(pheno_j, Observed),
       ETA = eta_i,
       nIter = iter,
       burnIn = iter / 2,
@@ -238,10 +250,10 @@ mclapply(pred_seq, FUN = function(i) {
     )
     yhat <- mod_bglr$yHat
     sd_yhat <- mod_bglr$SD.yHat
-    res <- slice(pheno_j, j)
+    res <- dplyr::slice(pheno_j, j)
     res$yhat <- yhat[j]
     res$var_yhat <- sd_yhat[j] ^ 2
-    res$Observed <- pheno_i %>% slice(j) %>% pull(Observed)
+    res$Observed <- pheno_i %>% dplyr::slice(j) %>% dplyr::pull(Observed)
 
     # Return the predictions.
     res
@@ -250,14 +262,14 @@ mclapply(pred_seq, FUN = function(i) {
   # Get the ID of the current scenario to reference it appropriately for later
   # retrieval.
   row_id <- prediction_template %>%
-    slice(i) %>%
-    pull(rowid)
+    dplyr::slice(i) %>%
+    dplyr::pull(rowid)
 
   one_param_df <- one_param_lst %>%
-    bind_rows() %>%
-    select(Genotype, Observed, yhat, var_yhat) %>%
-    bind_cols(slice(template_i, rep(1:n(), length(one_param_lst)))) %>%
-    mutate(rowid = row_id)
+    dplyr::bind_rows() %>%
+    dplyr::select(Genotype, Observed, yhat, var_yhat) %>%
+    dplyr::bind_cols(slice(template_i, rep(1:n(), length(one_param_lst)))) %>%
+    dplyr::mutate(rowid = row_id)
 
 
   # Save output and link it with a log file.
