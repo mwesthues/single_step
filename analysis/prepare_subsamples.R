483c5535488015a0386ab0322906bf627aebc372
westhues <matthias.westhues@uni-hohenheim.de>
2017-08-03 09:32:52 +0200

    Init level1 randomization fix (#4)

--- analysis/prepare_subsamples.R
+++ analysis/prepare_subsamples.R
@@ -1,5 +1,5 @@
-## Goal: 
-# Sample core sets of genotypes for the Yan-lab data in increments of ten 
+## Goal:
+# Sample core sets of genotypes for the Yan-lab data in increments of ten
 # percentage points.
 
 ## Topics
@@ -42,18 +42,18 @@ named_df$Predictor <- gsub(named_df$Predictor,
                            pattern = "_path",
                            replacement = "")
 named_df <- named_df %>%
-  as_data_frame() %>%
-  mutate(Path = as.character(Path))
+  tibble::as_data_frame() %>%
+  dplyr::mutate(Path = as.character(Path))
 
 pred_lst <- named_df %>%
-  as_data_frame() %>%
-  select(Path) %>%
-  flatten_chr() %>%
-  map(readRDS)
+  tibble::as_data_frame() %>%
+  dplyr::select(Path) %>%
+  purrr::flatten_chr() %>%
+  purrr::map(readRDS)
 
 pred_lst_names <- named_df %>%
-  select(Predictor) %>%
-  flatten_chr()
+  dplyr::select(Predictor) %>%
+  purrr::flatten_chr()
 names(pred_lst) <- pred_lst_names
 saveRDS(pred_lst, "./data/derived/predictor_subsets/pred_lst.RDS")
 
@@ -64,37 +64,37 @@ cluster14 <- readRDS("./data/derived/maizego/cluster_14_genotypes.RDS")
 
 ### hybrids
 hyb_pheno <- pred_lst %>%
-  keep(names(.) == "hyb_agro") %>%
+  purrr::keep(names(.) == "hyb_agro") %>%
   .[[1]]
 
 full_hyb_genos <- hyb_pheno %>%
-  pull(Genotype) %>%
-  unique()
+  dplyr::pull(Genotype) %>%
+  base::unique()
 
 core_hyb_parents <- pred_lst %>%
-  keep(names(.) == "hyb_mrna") %>%
+  purrr::keep(names(.) == "hyb_mrna") %>%
   .[[1]] %>%
   rownames()
 
 core_hyb_genos <- hyb_pheno %>%
-  separate(Genotype, into = c("Prefix", "Dent", "Flint"), sep = "_") %>%
-  filter(Dent %in% core_hyb_parents & Flint %in% core_hyb_parents) %>%
-  unite(Hybrid, Dent, Flint, sep = "_") %>%
-  mutate(Hybrid = paste0("DF_", Hybrid)) %>%
-  select(Hybrid) %>%
-  unique() %>%
-  flatten_chr()
+  tidyr::separate(Genotype, into = c("Prefix", "Dent", "Flint"), sep = "_") %>%
+  dplyr::filter(Dent %in% core_hyb_parents & Flint %in% core_hyb_parents) %>%
+  tidyr::unite(Hybrid, Dent, Flint, sep = "_") %>%
+  dplyr::mutate(Hybrid = paste0("DF_", Hybrid)) %>%
+  dplyr::select(Hybrid) %>%
+  base::unique() %>%
+  purrr::flatten_chr()
 
 ### inbred lines
-full_inb_bc <- pred_lst %>% 
-  keep(names(.) == "inb_snp") %>% 
-  .[[1]] %>% 
+full_inb_bc <- pred_lst %>%
+  purrr::keep(names(.) == "inb_snp") %>%
+  .[[1]] %>%
   rownames()
 
 full_inb_a <- intersect(full_inb_bc, cluster14)
 
 core_inb_bc <- pred_lst %>%
-  keep(names(.) == "inb_mrna") %>%
+  purrr::keep(names(.) == "inb_mrna") %>%
   .[[1]] %>%
   rownames()
 core_inb_a <- intersect(core_inb_bc, cluster14)
@@ -113,42 +113,50 @@ geno_lst <- list(
 
 geno_df <- geno_lst %>%
   utils::stack(drop = TRUE) %>%
-  separate(ind, into = c("Extent", "Material", "Scenario"), sep = "_") %>%
-  rename(G = "values") %>%
-  as_data_frame()
+  tidyr::separate(
+    ind,
+    into = c("Extent", "Material", "Scenario"),
+    sep = "_"
+  ) %>%
+  dplyr::rename(G = "values") %>%
+  tibble::as_data_frame()
 saveRDS(geno_df, "./data/derived/predictor_subsets/geno_df.RDS")
 
 # data frame with all combinations that need to be tested
 geno_param_df <- geno_df %>%
-  select(-G) %>%
-  unique()
+  dplyr::select(-G) %>%
+  base::unique()
 
 
 
 sample_by_combination <- function(i) {
 
-  param_subset <- slice(geno_param_df, i)
+  param_subset <- dplyr::slice(geno_param_df, i)
 
-  material_i <- geno_param_df %>% 
-    slice(i) %>%
-    pull(Material)
+  material_i <- geno_param_df %>%
+    dplyr::slice(i) %>%
+    dplyr::pull(Material)
 
   param_genos <- geno_df %>%
-    inner_join(
+    dplyr::inner_join(
       y = param_subset,
       by = c("Extent", "Material", "Scenario")
     ) %>%
-    pull(G) %>%
+    dplyr::pull(G) %>%
     gsub(pattern = "DF_", x = ., replacement = "")
-  
+
   loocv_samples <- sample_loo_sets(
   geno = param_genos,
   frac = 0.7,
   material = material_i,
   iter = 50
-  ) %>%
-  mutate(TST_Geno = paste0("DF_", TST_Geno))
-  loocv_samples
+  )
+  if (isTRUE(material_i == "Hybrid")) {
+    loocv_samples %>%
+    dplyr::mutate(TST_Geno = paste0("DF_", TST_Geno))
+  } else if (isTRUE(material_i == "Inbred")) {
+    loocv_samples
+  }
 }
 
 
@@ -156,19 +164,23 @@ sample_by_combination <- function(i) {
 rnd_level2_df <- geno_param_df %>%
   nrow() %>%
   seq_len() %>%
-  map(sample_by_combination) %>%
+  purrr::map(sample_by_combination) %>%
   purrr::set_names(nm = names(geno_lst)) %>%
-  bind_rows(.id = "ID") %>%
-  separate(ID, into = c("Extent", "Material", "Scenario"), sep = "_") %>%
-  rename(Rnd_Level2 = "Iter")
+  dplyr::bind_rows(.id = "ID") %>%
+  tidyr::separate(
+    ID,
+    into = c("Extent", "Material", "Scenario"),
+    sep = "_"
+  ) %>%
+  dplyr::rename(Rnd_Level2 = "Iter")
 
 # Save the result in two separate data frames to reduce memory requirements when
 # loading only a subset.
 rnd_level2_df %>%
-  filter(Rnd_Level2 %in% seq_len(25)) %>%
+  dplyr::filter(Rnd_Level2 %in% seq_len(25)) %>%
   saveRDS(., "./data/derived/predictor_subsets/rnd_level2_1-25.RDS")
 rnd_level2_df %>%
-  filter(Rnd_Level2 %in% seq(from = 26, to = 50, by = 1)) %>%
+  dplyr::filter(Rnd_Level2 %in% seq(from = 26, to = 50, by = 1)) %>%
   saveRDS(.,  "./data/derived/predictor_subsets/rnd_level2_26-50.RDS")
 
 
@@ -181,46 +193,61 @@ rnd_level2_df %>%
 ## -- LEVEL 1 SAMPLING ----------------------------------------------------
 # level 1 sampling is only required for the core set of inbred lines from the
 # maize diversity panel
-core_fractions <- seq(from = 0.1, to = 0.9, by = 0.1)
+level2_geno_df <- rnd_level2_df %>%
+  dplyr::filter(Extent == "Core", Material == "Inbred") %>%
+  dplyr::select(-Extent, -Material)
+
 level1_frame <- geno_df %>%
   filter(Extent == "Core", Material == "Inbred") %>%
   pull(Scenario) %>%
   unique()
 
-level1_geno_df <- geno_df %>%
-  filter(Extent == "Core", Material == "Inbred") %>%
-  select(G, Scenario)
-
+# Sample a fraction of training set individuals based on the core set size,
+# which is specified as a fraction of 1.
 sample_core <- function(x) {
-  geno <- pull(x, G)
   smp_fraction <- x %>%
-    pull(Core_Fraction) %>%
-    unique()
+    dplyr::pull(Core_Fraction) %>%
+    base::unique()
   sample_frac(x, size = smp_fraction)
 }
 
-rnd_level1_df <- level1_frame %>%
-  expand.grid(
-    Extent = "Core",
-    Material = "Inbred",
-    Scenario = .,
-    Core_Fraction = core_fractions,
-    Rnd_Level1 = seq_len(50),
-    stringsAsFactors = FALSE
-  ) %>%
-  as_data_frame() %>%
-  right_join(., y = level1_geno_df, by = "Scenario") %>%
-  split(list(.$Scenario, .$Core_Fraction, .$Rnd_Level1)) %>%
-  map(sample_core) %>%
-  bind_rows()
 
-# Add all other combinations to the data frame. This will facilitate the setup
-# of ETA objects later on.
-rnd_level1_df <- geno_df %>%
-  mutate(Core_Fraction = 0, Rnd_Level1 = 0) %>%
-  bind_rows(., rnd_level1_df) %>%
-  mutate_all(as.character)
-saveRDS(rnd_level1_df, "./data/derived/predictor_subsets/rnd_level1.RDS")
+core_fractions <- seq(from = 0.1, to = 0.9, by = 0.1)
+lapply(core_fractions, FUN = function(i) {
+  print(i)
+  level1_frame %>%
+    expand.grid(
+      Extent = "Core",
+      Material = "Inbred",
+      Scenario = .,
+      Core_Fraction = i,
+      Rnd_Level1 = seq_len(50),
+      stringsAsFactors = FALSE
+    ) %>%
+    tibble::as_data_frame() %>%
+    dplyr::right_join(
+      x = .,
+      y = level2_geno_df,
+      by = "Scenario"
+    ) %>%
+    base::split(list(.$Scenario, .$Rnd_Level1, .$Rnd_Level2, .$TST_Geno), drop = TRUE) %>%
+    purrr::map(sample_core) %>%
+    dplyr::bind_rows() %>%
+    saveRDS(
+      object = .,
+      file = paste0("./data/derived/predictor_subsets/core_set", i, ".RDS")
+    )
+})
+
+
+
+## Add all other combinations to the data frame. This will facilitate the setup
+## of ETA objects later on.
+#rnd_level1_df <- geno_df %>%
+#  dplyr::mutate(Core_Fraction = 0, Rnd_Level1 = 0) %>%
+#  dplyr::bind_rows(., rnd_level1_df) %>%
+#  dplyr::mutate_all(as.character)
+#saveRDS(rnd_level1_df, "./data/derived/predictor_subsets/rnd_level1.RDS")
 
 
 
@@ -312,8 +339,8 @@ spec_eta_df <- list(
   eta_spec6,
   eta_spec7
   ) %>%
-  bind_rows() %>%
-  as_data_frame()
+  dplyr::bind_rows() %>%
+  tibble::as_data_frame()
 saveRDS(spec_eta_df, "./data/derived/predictor_subsets/spec_eta_df.RDS")
 
 
@@ -329,36 +356,36 @@ scenario_seq <- spec_eta_df %>%
   seq_len()
 
 lapply(scenario_seq, FUN = function(i) {
-  scen_i <- slice(spec_eta_df, i)
-  pred1 <- pull(scen_i, Pred1)
-  pred2 <- pull(scen_i, Pred2)
-  extent <- pull(scen_i, Extent)
+  scen_i <- dplyr::slice(spec_eta_df, i)
+  pred1 <- dplyr::pull(scen_i, Pred1)
+  pred2 <- dplyr::pull(scen_i, Pred2)
+  extent <- dplyr::pull(scen_i, Extent)
   pred_model <- "BRR"
-  if (pull(scen_i, Material) == "Hybrid") {
+  if (dplyr::pull(scen_i, Material) == "Hybrid") {
     material_type <- "hyb"
   } else {
     material_type <- "inb"
   }
   
   pred_combi <- list(pred1, pred2) %>%
-     keep(nchar(.) != 0) %>%
-     flatten_chr() %>%
+     purrr::keep(nchar(.) != 0) %>%
+     purrr::flatten_chr() %>%
      paste(., collapse = "_")
    
    pred_sets <- pred_combi %>%
      strsplit(split = "_") %>%
-     flatten_chr()
+     purrr::flatten_chr()
 
   pred_lst_selector <- paste(material_type, pred_sets, sep = "_")
 
   # Get the random core set of genotypes covering the genetic target space
   # of a pre-specified size (as a fraction of genotypes covered by mRNA data).
-  tmp_geno <- inner_join(
+  tmp_geno <- dplyr::inner_join(
     rnd_level1_df,
     scen_i,
     by = c("Extent", "Material", "Scenario", "Core_Fraction", "Rnd_Level1")
   ) %>%
-  pull(G)
+  dplyr::pull(G)
 
   if (isTRUE(nchar(pred2) == 0 && pred1 != "mrna")) {
     full_geno <- tmp_geno
@@ -366,29 +393,29 @@ lapply(scenario_seq, FUN = function(i) {
   } else if (pred1 == "mrna" && pred2 == "" && extent == "Full") {
     full_geno <- tmp_geno
     core_geno <- scen_i %>%
-      mutate(Extent = "Core") %>%
-      inner_join(
+      dplyr::mutate(Extent = "Core") %>%
+      dplyr::inner_join(
         rnd_level1_df,
         by = c("Extent", "Material", "Scenario", "Core_Fraction", "Rnd_Level1")
       ) %>%
-      pull(G)
+      dplyr::pull(G)
   } else if (all(nchar(pred_sets) != 0) && extent == "Core") {
     core_geno <- tmp_geno
-    full_geno <- inner_join(
+    full_geno <- dplyr::inner_join(
       geno_df,
       scen_i,
       by = c("Extent", "Material", "Scenario")
     ) %>%
-    pull(G)
+    dplyr::pull(G)
   } else if (all(nchar(pred_sets) != 0) && extent == "Full") {
     full_geno <- tmp_geno
     core_geno <- scen_i %>%
-      mutate(Extent = "Core") %>%
-      inner_join(
+      dplyr::mutate(Extent = "Core") %>%
+      dplyr::inner_join(
         rnd_level1_df,
         by = c("Extent", "Material", "Scenario", "Core_Fraction", "Rnd_Level1")
       ) %>%
-      pull(G)
+      dplyr::pull(G)
   } 
 
 
@@ -397,17 +424,17 @@ lapply(scenario_seq, FUN = function(i) {
   # have only polymorphic markers and no markers in perfect LD.
   if (isTRUE(pull(scen_i, Material) == "Inbred")) {
     curr_pred_lst <- pred_lst %>%
-      keep(names(.) %in% pred_lst_selector) %>%
-      set_names(
+      purrr::keep(names(.) %in% pred_lst_selector) %>%
+      purrr::set_names(
         nm = gsub(pattern = "^[^_]*_", replacement = "", x = names(.))
       ) %>%
-      modify_at("mrna", .f = function(x) {
+      purrr::modify_at("mrna", .f = function(x) {
         x[rownames(x) %in% core_geno, ]
       }) %>%
-      modify_at("snp", .f = function(x) {
+      purrr::modify_at("snp", .f = function(x) {
         x[rownames(x) %in% full_geno, ]
       }) %>%
-      modify_at("snp",
+      purrr::modify_at("snp",
         .f = ~sspredr::ensure_snp_quality(
           ., callfreq_check = FALSE, maf_check = TRUE, maf_threshold = 0.05,
           any_missing = FALSE, remove_duplicated = TRUE
@@ -416,17 +443,21 @@ lapply(scenario_seq, FUN = function(i) {
   }
 
 
-  if (isTRUE(pull(scen_i, Material) == "Hybrid")) {
+  if (isTRUE(dplyr::pull(scen_i, Material) == "Hybrid")) {
     hybrid_parents <- full_geno %>%
-      as_data_frame() %>%
-      rename(Genotype = "value") %>%
-      separate(Genotype, into = c("Prefix", "Dent", "Flint"), sep = "_") %>%
-      select(Dent, Flint) %>%
+      tibble::as_data_frame() %>%
+      dplyr::rename(Genotype = "value") %>%
+      tidyr::separate(
+        Genotype,
+        into = c("Prefix", "Dent", "Flint"),
+        sep = "_"
+      ) %>%
+      dplyr::select(Dent, Flint) %>%
       unique() %>%
-      gather(key = Pool, value = Genotype) %>%
-      split(.$Pool) %>%
-      map(., ~select(., Genotype)) %>%
-      map(flatten_chr)
+      tidyr::gather(key = Pool, value = Genotype) %>%
+      base::split(.$Pool) %>%
+      purrr::map(., ~select(., Genotype)) %>%
+      purrr::map(flatten_chr)
   
     hybrid_names <- full_geno
 
@@ -441,15 +472,15 @@ lapply(scenario_seq, FUN = function(i) {
     # for each heterotic group in order to have only polymorphic markers and no 
     # markers in perfect LD.
     curr_pred_lst <- pred_lst %>%
-      keep(names(.) %in% pred_lst_selector) %>%
-      set_names(
+      purrr::keep(names(.) %in% pred_lst_selector) %>%
+      purrr::set_names(
         nm = gsub(pattern = "^[^_]*_", replacement = "", x = names(.))
       ) %>%
-      modify_if(.p = names(.) == "ped",
+      purrr::modify_if(.p = names(.) == "ped",
              .f = split_into_hetgroups, y = hybrid_parents, pedigree = TRUE) %>%
-      modify_if(.p = names(.) != "ped",
+      purrr::modify_if(.p = names(.) != "ped",
              .f = split_into_hetgroups, y = hybrid_parents, pedigree = FALSE) %>%
-      modify_at("snp", .f = ~map(., ~sspredr::ensure_snp_quality(
+      purrr::modify_at("snp", .f = ~map(., ~sspredr::ensure_snp_quality(
         ., callfreq_check = FALSE, maf_check = TRUE,
         maf_threshold = 0.05, any_missing = FALSE, remove_duplicated = TRUE
         )
@@ -469,7 +500,7 @@ lapply(scenario_seq, FUN = function(i) {
       purrr::transpose() %>%
       add_parental_hybrid_names()
   
-  } else if (isTRUE(pull(scen_i, Material) == "Inbred")) {
+  } else if (isTRUE(dplyr::pull(scen_i, Material) == "Inbred")) {
 
     pre_eta <- list(Inbred = curr_pred_lst)
     pre_eta[[1]][["geno"]] <- full_geno
@@ -479,9 +510,9 @@ lapply(scenario_seq, FUN = function(i) {
   # Determine the number of predictors to decide later on, which function to
   # call for the set-up of the kernels.
   raw_args <- pre_eta %>%
-    map(names) %>%
-    reduce(intersect) %>%
-    discard(. == "geno")
+    purrr::map(names) %>%
+    purrr::reduce(intersect) %>%
+    purrr::discard(. == "geno")
   pred_number <- raw_args %>%
     .[grep("ped|snp|mrna", x = .)] %>%
     length()
@@ -494,14 +525,14 @@ lapply(scenario_seq, FUN = function(i) {
   # 'know' if the predictor matrices need to be scaled or not.
   if (all(grepl("ped", x = raw_args))) {
     pre_eta <- pre_eta %>%
-      map(.f = function(x) {
+      purrr::map(.f = function(x) {
         x$as_kernel <- FALSE
         x$bglr_model <- pred_model
         x
     })
   } else if (any(grepl("ped", x = raw_args))) {
     pre_eta <- pre_eta %>%
-      map(.f = function(x) {
+      purrr::map(.f = function(x) {
         x$is_pedigree <- TRUE
         x$bglr_model <- pred_model
         x$as_kernel <- TRUE
@@ -509,7 +540,7 @@ lapply(scenario_seq, FUN = function(i) {
       })
   } else {
     pre_eta <- pre_eta %>%
-      map(.f = function(x) {
+      purrr::map(.f = function(x) {
         x$is_pedigree <- FALSE
         x$bglr_model <- pred_model
         x$as_kernel <- TRUE
@@ -531,17 +562,17 @@ lapply(scenario_seq, FUN = function(i) {
   eta_pred_nms <- eta_fun_args %>%
     .[!grepl("as_kernel|is_pedigree|geno|bglr_model", x = .)]
   current_eta_names <- pre_eta %>%
-    map(names) %>%
-    reduce(intersect)
+    purrr::map(names) %>%
+    purrr::reduce(intersect)
   current_eta_names[seq_len(pred_number)] <- eta_pred_nms
   pre_eta <- pre_eta %>%
-    map(function(x) {
+    purrr::map(function(x) {
       names(x) <- current_eta_names
       x
     })
   # Build the kernel for BGLR.
-  eta <- invoke_map(get(eta_fun), pre_eta) %>%
-    flatten()
+  eta <- purrr::invoke_map(get(eta_fun), pre_eta) %>%
+    purrr::flatten()
 
   ## Save individual files.
   data_location <- paste0("./data/derived/predictor_subsets/eta_", i, ".RDS")
@@ -553,11 +584,11 @@ lapply(scenario_seq, FUN = function(i) {
 
 # Safe the log file.
 log_df <- scenario_seq %>%
-  map(function(i) {
-    slice(spec_eta_df, i)
+  purrr::map(function(i) {
+    dplyr::slice(spec_eta_df, i)
   }) %>%
-  bind_rows() %>%
-  mutate(Data_Location = paste0(
+  dplyr::bind_rows() %>%
+  dplyr::mutate(Data_Location = paste0(
     "./data/derived/predictor_subsets/eta_",
     scenario_seq,
     ".RDS"
