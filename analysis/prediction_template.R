d6189252ba8ab1784367ec96f13858958163a1f8
westhues <matthias.westhues@uni-hohenheim.de>
2017-08-20 16:57:30 +0200

    Define pred cluster (closes #5)

--- analysis/prediction_template.R
+++ analysis/prediction_template.R
@@ -299,62 +299,6 @@ if (length(list.files(main_dir, pattern = "hybrid_.{36}\\.RDS")) == 0) {
 
 
 
-## -- ALL COMBINATIONS ----------------------------------------------------
-template_precursor <- rnd_level2_df %>%
-  select(-TST_Geno, -TRN_Geno, -ind) %>%
-  unique() %>%
-  full_join(y = spec_eta_df, by = c("Extent", "Material", "Scenario"))
-
-rm(rnd_level2_df, spec_eta_df)
-
-# Get the number of entries per hybrid and per inbred, respectively.
-# Then, augment each material group so that every scenario contains the multiple
-# traits.
-n_hyb <- template_precursor %>%
-  filter(Material == "Hybrid") %>%
-  nrow()
-
-n_inb <- template_precursor %>%
-  filter(Material == "Inbred") %>%
-  nrow()
-
-hyb_template <- template_precursor %>%
-  filter(Material == "Hybrid") %>%
-  slice(rep(1:n(), each = length(hyb_traits))) %>%
-  mutate(Trait = rep(hyb_traits, times = n_hyb))
-
-inb_template <- template_precursor %>%
-  filter(Material == "Inbred") %>%
-  slice(rep(1:n(), each = length(inb_traits))) %>%
-  mutate(Trait = rep(inb_traits, times = n_inb))
-
-
-template_lst <- inb_template %>%
-  bind_rows(., hyb_template) %>%
-  mutate_at(c("Rnd_Level1", "Rnd_Level2"), as.numeric) %>%
-  arrange(
-    Material,
-    Extent,
-    Scenario,
-    Pred1,
-    Pred2,
-    Core_Fraction,
-    Rnd_Level1,
-    Rnd_Level2,
-    Trait
-  ) %>%
-  rowid_to_column() %>%
-  filter(
-    Rnd_Level1 %in% seq(from = 0, to = 25, by = 1),
-    Rnd_Level2 %in% seq(from = 0, to = 25, by = 1)
-  ) %>%
-  mutate_at(.vars = c("Extent", "Material", "Scenario"), .funs = as.factor) %>%
-  split(list(.$Extent, .$Material, .$Scenario), drop = TRUE) %>%
-  keep(~ nrow(.x) != 0)
-
-
-
-
 ## -- FUNCTIONS -------------------------------------------------------------
 cut_data <- function(job_df, minutes_per_job, minutes_per_node, tolerance) {
   #---
@@ -395,18 +339,36 @@ cut_data <- function(job_df, minutes_per_job, minutes_per_node, tolerance) {
 
 
 
-
-
-
 ## -- GENERATE PREDICTION JOB INTERVALS -------------------------------------
 # Time per node (hours * minutes/hour * cores)
 node_time_per_day <- 24 * 60 * 16
 # Time per job in minutes
-job_minutes <- c(18, 25, 25, 35, 105, 3200)
-names(job_minutes) <- names(template_lst)
+job_minutes <- c(
+  "CIA" = 18,
+  "CIB" = 25,
+  "FIA" = 25,
+  "FIB" = 35,
+  "CHN" = 105,
+  "FHN" = 3200
+)
+
+template_lst <- full_scen_df %>%
+  dplyr::arrange(
+    Combi,
+    Predictor,
+    Core_Fraction,
+    Rnd_Level1,
+    Rnd_Level2,
+    Trait
+  ) %>%
+  split(list(.$Combi)) %>%
+  .[match(names(job_minutes), names(.))]
+
+
+
 # Fractions of the day times number of minutes per node and day
 template_node_time <- c(0.5, 1, 1, 1, 1, 2.5) * node_time_per_day
-names(template_node_time) <- names(template_lst)
+names(template_node_time) <- names(job_minutes)
 
 pre_map_lst <- list(
     job_df = template_lst,
@@ -415,26 +377,18 @@ pre_map_lst <- list(
     tolerance = rep(1.3, times = length(template_lst))
 )
 
-# Create short identifiers for each of the six blocks that need to be analyzed
-# to ensure that the intervals are unique.
-combi_names <- template_lst %>%
-  names() %>%
-  strsplit(., split = "[.]") %>%
-  map(~ substr(., start = 1, stop = 1)) %>%
-  map_chr(~ paste(., collapse = ""))
-
 prediction_template <- pmap(. = pre_map_lst, .f = cut_data) %>%
-  set_names(., nm = combi_names) %>%
-  bind_rows(.id = "Combi") %>%
-  mutate(Interval = paste(Combi, Interval, sep = "_"))
+  purrr::set_names(., nm = names(job_minutes)) %>%
+  dplyr::bind_rows() %>%
+  dplyr::mutate(Interval = paste(Combi, Interval, sep = "_"))
 
 # Check how many nodes will be requested for each of the six blocks.
 prediction_template %>%
-  mutate(Combi = as.factor(Combi)) %>%
+  dplyr::mutate(Combi = as.factor(Combi)) %>%
   split(.$Combi) %>%
-  map(~ pull(., Interval)) %>%
-  map(unique) %>%
-  map_int(length)
+  purrr::map(~dplyr::pull(., Interval)) %>%
+  purrr::map(unique) %>%
+  purrr::map_int(length)
 
 
 pred_loc <- "./data/derived/prediction_runs/"
